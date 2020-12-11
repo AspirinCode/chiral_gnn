692170055d5c3624b895ccce2b2985679ae63b04
hwpang <hwpang@mit.edu>
2020-12-11 17:38:08 -0500

    Add different permuters and adjust for different num of neighbors

--- model/tetra.py
+++ model/tetra.py
@@ -9,7 +9,15 @@ class TetraPermuter(nn.Module):
     def __init__(self, hidden, device):
         super(TetraPermuter, self).__init__()
 
-        self.W_bs = nn.ModuleList([copy.deepcopy(nn.Linear(hidden, hidden)) for _ in range(4)])
+        self.n_nums = dict()
+        self.n_nums["di"] = 2
+        self.n_nums["tri"] = 3
+        self.n_nums["tetra_nonchiral"] = 4
+        self.n_nums["tetra_chiral"] = 4
+        
+        self.W_bs = dict()
+        for n_neighbor, num in self.n_nums.items():
+            self.W_bs[n_neighbor] = nn.ModuleList([copy.deepcopy(nn.Linear(hidden, hidden)) for _ in range(num)])
         self.device = device
         self.drop = nn.Dropout(p=0.2)
         self.reset_parameters()
@@ -17,31 +25,65 @@ class TetraPermuter(nn.Module):
                                      nn.BatchNorm1d(hidden),
                                      nn.ReLU(),
                                      nn.Linear(hidden, hidden))
-
-        self.tetra_perms = torch.tensor([[0, 1, 2, 3],
-                                         [0, 2, 3, 1],
-                                         [0, 3, 1, 2],
-                                         [1, 0, 3, 2],
-                                         [1, 2, 0, 3],
-                                         [1, 3, 2, 0],
-                                         [2, 0, 1, 3],
-                                         [2, 1, 3, 0],
-                                         [2, 3, 0, 1],
-                                         [3, 0, 2, 1],
-                                         [3, 1, 0, 2],
-                                         [3, 2, 1, 0]])
+            
+        self.perms = dict()
+        self.perms["di"] = torch.tensor([[0, 1],
+                                       [1, 0]])
+        self.perms["tri"] = torch.tensor([[0, 1, 2],
+                                        [0, 2, 1],
+                                        [1, 0, 2],
+                                        [1, 2, 0],
+                                        [2, 0, 1],
+                                        [2, 1, 0]])
+        self.perms["tetra_chiral"] = torch.tensor([[0, 1, 2, 3],
+                                                 [0, 2, 3, 1],
+                                                 [0, 3, 1, 2],
+                                                 [1, 0, 3, 2],
+                                                 [1, 2, 0, 3],
+                                                 [1, 3, 2, 0],
+                                                 [2, 0, 1, 3],
+                                                 [2, 1, 3, 0],
+                                                 [2, 3, 0, 1],
+                                                 [3, 0, 2, 1],
+                                                 [3, 1, 0, 2],
+                                                 [3, 2, 1, 0]])
+        self.perms["tetra_nonchiral"] = torch.tensor([[0, 1, 2, 3],
+                                                    [0, 1, 3, 2],
+                                                    [0, 2, 1, 3],
+                                                    [0, 2, 3, 1],
+                                                    [0, 3, 1, 2],
+                                                    [0, 3, 2, 1],
+                                                    [1, 0, 2, 3],
+                                                    [1, 0, 3, 2],
+                                                    [1, 2, 3, 0],
+                                                    [1, 2, 0, 3],
+                                                    [1, 3, 0, 2],
+                                                    [1, 3, 2, 0],
+                                                    [2, 0, 1, 3],
+                                                    [2, 0, 3, 1],
+                                                    [2, 1, 0, 3],
+                                                    [2, 1, 3, 0],
+                                                    [2, 3, 0, 1],
+                                                    [2, 3, 1, 0],
+                                                    [3, 0, 1, 2],
+                                                    [3, 0, 2, 1],
+                                                    [3, 1, 0, 2],
+                                                    [3, 1, 2, 0],
+                                                    [3, 2, 0, 1],
+                                                    [3, 2, 1, 0]])
 
     def reset_parameters(self):
-        gain = 0.5
-        for W_b in self.W_bs:
-            nn.init.xavier_uniform_(W_b.weight, gain=gain)
-            gain += 0.5
+        for n_neighbor, W_bs in self.W_bs.items():
+            gain = 0.5
+            for W_b in W_bs:
+                nn.init.xavier_uniform_(W_b.weight, gain=gain)
+                gain += 0.5
 
-    def forward(self, x):
+    def forward(self, x, n_neighbor):
         nei_messages = torch.zeros([x.size(0), x.size(2)]).to(self.device)
 
-        for p in self.tetra_perms:
-            nei_messages_list = [self.drop(F.tanh(l(t))) for l, t in zip(self.W_bs, torch.split(x[:, p, :], 1, dim=1))]
+        for p in self.perms[n_neighbor]:
+            nei_messages_list = [self.drop(F.tanh(l(t))) for l, t in zip(self.W_bs[n_neighbor], torch.split(x[:, p, :], 1, dim=1))]
             nei_messages += self.drop(F.relu(torch.cat(nei_messages_list, dim=1).sum(dim=1)))
         return self.mlp_out(nei_messages / 3.)
 
@@ -51,8 +93,16 @@ class ConcatTetraPermuter(nn.Module):
     def __init__(self, hidden, device):
         super(ConcatTetraPermuter, self).__init__()
 
-        self.W_bs = nn.Linear(hidden*4, hidden)
-        torch.nn.init.xavier_normal_(self.W_bs.weight, gain=1.0)
+        self.n_nums = dict()
+        self.n_nums["di"] = 2
+        self.n_nums["tri"] = 3
+        self.n_nums["tetra_nonchiral"] = 4
+        self.n_nums["tetra_chiral"] = 4
+        
+        self.W_bs = dict()
+        for n_neighbor, num in self.n_nums.items():
+            self.W_bs[n_neighbor] = nn.Linear(hidden*num, hidden)
+            torch.nn.init.xavier_normal_(self.W_bs[n_neighbor].weight, gain=1.0)
         self.hidden = hidden
         self.device = device
         self.drop = nn.Dropout(p=0.2)
@@ -61,25 +111,58 @@ class ConcatTetraPermuter(nn.Module):
                                      nn.ReLU(),
                                      nn.Linear(hidden, hidden))
 
-        self.tetra_perms = torch.tensor([[0, 1, 2, 3],
-                                         [0, 2, 3, 1],
-                                         [0, 3, 1, 2],
-                                         [1, 0, 3, 2],
-                                         [1, 2, 0, 3],
-                                         [1, 3, 2, 0],
-                                         [2, 0, 1, 3],
-                                         [2, 1, 3, 0],
-                                         [2, 3, 0, 1],
-                                         [3, 0, 2, 1],
-                                         [3, 1, 0, 2],
-                                         [3, 2, 1, 0]])
+        self.perms = dict()
+        self.perms["di"] = torch.tensor([[0, 1],
+                                       [1, 0]])
+        self.perms["tri"] = torch.tensor([[0, 1, 2],
+                                        [0, 2, 1],
+                                        [1, 0, 2],
+                                        [1, 2, 0],
+                                        [2, 0, 1],
+                                        [2, 1, 0]])
+        self.perms["tetra_chiral"] = torch.tensor([[0, 1, 2, 3],
+                                                 [0, 2, 3, 1],
+                                                 [0, 3, 1, 2],
+                                                 [1, 0, 3, 2],
+                                                 [1, 2, 0, 3],
+                                                 [1, 3, 2, 0],
+                                                 [2, 0, 1, 3],
+                                                 [2, 1, 3, 0],
+                                                 [2, 3, 0, 1],
+                                                 [3, 0, 2, 1],
+                                                 [3, 1, 0, 2],
+                                                 [3, 2, 1, 0]])
+        self.perms["tetra_nonchiral"] = torch.tensor([[0, 1, 2, 3],
+                                                    [0, 1, 3, 2],
+                                                    [0, 2, 1, 3],
+                                                    [0, 2, 3, 1],
+                                                    [0, 3, 1, 2],
+                                                    [0, 3, 2, 1],
+                                                    [1, 0, 2, 3],
+                                                    [1, 0, 3, 2],
+                                                    [1, 2, 3, 0],
+                                                    [1, 2, 0, 3],
+                                                    [1, 3, 0, 2],
+                                                    [1, 3, 2, 0],
+                                                    [2, 0, 1, 3],
+                                                    [2, 0, 3, 1],
+                                                    [2, 1, 0, 3],
+                                                    [2, 1, 3, 0],
+                                                    [2, 3, 0, 1],
+                                                    [2, 3, 1, 0],
+                                                    [3, 0, 1, 2],
+                                                    [3, 0, 2, 1],
+                                                    [3, 1, 0, 2],
+                                                    [3, 1, 2, 0],
+                                                    [3, 2, 0, 1],
+                                                    [3, 2, 1, 0]])
 
-    def forward(self, x):
+    def forward(self, x, n_neighbor):
 
         nei_messages = torch.zeros([x.size(0), x.size(2)]).to(self.device)
 
-        for p in self.tetra_perms:
-            nei_messages += self.drop(F.relu(self.W_bs(x[:, p, :].view(x.size(0), self.hidden*4))))
+        for p in self.perms[n_neighbor]:
+            nei_messages += self.drop(F.relu(self.W_bs[n_neighbor](x[:, p, :].view(x.size(0), self.hidden*self.n_nums[n_neighbor]))))
         return self.mlp_out(nei_messages / 3.)
 
 
@@ -88,23 +171,30 @@ class TetraDifferencesProduct(nn.Module):
     def __init__(self, hidden):
         super(TetraDifferencesProduct, self).__init__()
 
+        self.n_nums = dict()
+        self.n_nums["di"] = 2
+        self.n_nums["tri"] = 3
+        self.n_nums["tetra_nonchiral"] = 4
+        self.n_nums["tetra_chiral"] = 4
+        
         self.mlp_out = nn.Sequential(nn.Linear(hidden, hidden),
                                      nn.BatchNorm1d(hidden),
                                      nn.ReLU(),
                                      nn.Linear(hidden, hidden))
 
-    def forward(self, x):
+    def forward(self, x, n_neighbor):
 
-        indices = torch.arange(4).to(x.device)
-        message_tetra_nbs = [x.index_select(dim=1, index=i).squeeze(1) for i in indices]
-        message_tetra = torch.ones_like(message_tetra_nbs[0])
+        num = self.n_nums[n_neighbor]
+        indices = torch.arange(num).to(x.device)
+        message_n_nbs = [x.index_select(dim=1, index=i).squeeze(1) for i in indices]
+        message_n = torch.ones_like(message_n_nbs[0])
 
         # note: this will zero out reps for chiral centers with multiple carbon neighbors on first pass
-        for i in range(4):
-            for j in range(i + 1, 4):
-                message_tetra = torch.mul(message_tetra, (message_tetra_nbs[i] - message_tetra_nbs[j]))
-        message_tetra = torch.sign(message_tetra) * torch.pow(torch.abs(message_tetra) + 1e-6, 1 / 6)
-        return self.mlp_out(message_tetra)
+        for i in range(num):
+            for j in range(i + 1, num):
+                message_n = torch.mul(message_n, (message_n_nbs[i] - message_n_nbs[j]))
+        message_n = torch.sign(message_n) * torch.pow(torch.abs(message_n) + 1e-6, 1 / 6)
+        return self.mlp_out(message_n)
 
 
 def get_tetra_update(args):
